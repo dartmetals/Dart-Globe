7175bdd0ef47d401afa152c9e6bb0720819a573d
sowmyakota-mv <mastersvisa.da31@gmail.com>
2026-02-24 15:28:25 +0530

    modified all countries

--- src/pages/Study-Abroad/AustraliaPage.tsx
+++ src/pages/Study-Abroad/AustraliaPage.tsx
@@ -57,7 +57,7 @@ const StudyAustraliaPage: React.FC = () => {
   return (
     <div className="bg-white">
       {/* Hero Section */}
-<section className="relative h-[40vh] sm:h-[76vh] md:h-[80vh] text-white overflow-hidden bg-white">
+<section className="relative h-[44vh] sm:h-[76vh] md:h-[80vh] text-white overflow-hidden bg-white">
   
   {/* Background Image Wrapper */}
   <div
