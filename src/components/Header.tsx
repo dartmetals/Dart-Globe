ee9d6db02a56f8b7520b316e513a184c53d9d766
sowmyakota-mv <mastersvisa.da31@gmail.com>
2026-04-03 10:04:07 +0530

    changed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -553,7 +553,7 @@ useEffect(() => {
             <div className="sticky top-0 z-10 bg-white border-b border-gray-200 px-6 py-4 flex justify-between items-center flex-shrink-0">
               <div className="flex items-center space-x-2">
                 <img 
-                  src="/dg-logo.png" 
+                  src="/dg-header1.png" 
                   alt="Dart Globe Logo" 
                   className="h-16 w-auto"
                 />
